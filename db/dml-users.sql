964c2f5be305d44623a1a22c2e161bfbb502a88f
MarioCos28 <mario.cosiguape28@gmail.com>
2025-09-02 22:19:07 -0600

    Creacion de dml

--- db/dml-users.sql
+++ db/dml-users.sql
@@ -1,16 +1,31 @@
-insert into users(
-                fullname, 
-                user_address,
-            ) 
-values (
-        "Juan perez",
-        "Avenida ejemplo 123"
-    )
+-- Ingreso de datos de los usuarios
+INSERT INTO users (
+  fullname, user_address, email, dpi, nit, rol, user_status, user_password
+) VALUES ('Mario cosiga', 'Zona 1, pinula', 'mario@example.com', '1234567890101', '1234567-8', 'admin', 1, 'mmm123'),
+  ('Omar ramos', 'zona 16, Ciudad de Guatemala', 'omar@example.com', '3216549870123', '4567890-1', 'admin', 2, 'oooo123'),
+  ('Omar ramos', 'zona 16, Ciudad de Guatemala', 'omar@example.com', '3216549870123', '4567890-1', 'admin', 2, 'oooo123'),
+  ('Alex estrada', 'Zona 16, Ciudad de Guatemala', 'alex@example.com', '3216549870123', '4567890-1', 'admin', 1, 'soyhueco456');
 
---  Obtener usuario por email (ejemplo, insertar datos reales para pruebas de consultas);
-select  id
-        fullname
-        user_address
-        email
-        -- ....
-    where email = "example@email.com"
+  -- Busqueda de usuarios
+select * from users
+where email = 'omar@example.com';
+
+-- Eliminacion de usuario seleccionado
+USE hotelesapp;
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM users WHERE email = 'omar@example.com';
+SET SQL_SAFE_UPDATES = 1;
+
+-- Vista unica de ID
+select id, fullname from users;
+
+-- Actualización de usuarios
+UPDATE users
+SET 
+  fullname = 'Mario Cosiga',
+  user_address = 'Zona 2, Santa Catarina Pinula',
+  user_status = 1
+WHERE email = 'mario@example.com';
+
+-- El status del usuario = 1
+select * from users u where u.user_status = 1
